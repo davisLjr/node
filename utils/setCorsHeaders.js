4caf5c75f5d89ba84cc345b2a5a8728d5795c1e4
Davis <davis.laviera@nonconformist.digital>
2025-06-11 15:44:35 +0200

    updating index.js products api

--- utils/setCorsHeaders.js
+++ utils/setCorsHeaders.js
@@ -5,12 +5,15 @@ export function setCorsHeaders(req, res) {
   ];
 
   const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
+
+  if (origin && allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
 
+  res.setHeader("Vary", "Origin");
+
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
 
   if (req.method === "OPTIONS") {
